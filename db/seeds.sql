0196af84c65f890afa036df93be952e52b21ecb4
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-04-20 19:42:01 -0500

    modifying seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,12 +5,12 @@ VALUES ("Admin"),
        ("Sales");
 
        INSERT INTO `role` (title, salary, department_id)
-VALUES ("Administrative Assistant", 60000, 1),
-       ("Sales Associate", 65000, 4),
-       ("Legal Intern", 100000, 2),
-       ("Sales Lead", 95000, 4),
-       ("Marketing Intern", 55000, 3),
-       ("Marketing Manager", 80000, 3);
+VALUES ("Administrative Assistant", 20000, 1),
+       ("Sales Associate", 35000, 4),
+       ("Legal Intern", 800000, 2),
+       ("Sales Lead", 45000, 4),
+       ("Marketing Intern", 77000, 3),
+       ("Marketing Manager", 340000, 3);
 
        
        INSERT INTO employee (first_name, last_name, role_id, manager_id)
