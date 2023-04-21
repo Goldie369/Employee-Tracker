d8bde31f4b4035a64893ee40c3b4ff43abc8e7fd
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-04-20 19:11:56 -0500

    adding questions roles

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ const cTable = require('console.table');
 
 const { greeting, color } = require('./Assets/greeting');
 
+const firstQuestion = [
+    {
+        type: 'list',
+        name: 'menu',
+        message: 'What would you like to do?',
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit']
+    },
+]
+
+
+
